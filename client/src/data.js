8017f3b8eb54d3d3764b6609750d918895119164
desi-coder11 <kanhaiyavishwakarma661@gmail.com>
2023-01-26 11:16:28 +0530

    first commit

---
+++ client/src/data.js
@@ -0,0 +1,90 @@
+export const sliderItems = [
+    {
+      id: 1,
+            img: "https://i.postimg.cc/nz1KyDJ6/pngfind-com-fashion-png-323913.png",
+
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img:"https://i.postimg.cc/QxbCLwrG/pngfind-com-taylor-swift-png-465463.png",
+
+      // img: "https://i.postimg.cc/5xV1qCrp/pngfind-com-model-png-524008.png",
+      // https://i.postimg.cc/ncw21dwZ/pngfind-com-fashion-png-323329.png
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.postimg.cc/hjx4vLj5/pngfind-com-fashion-png-434351.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+      cat: "women",
+
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+      cat: "coat",
+
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+      cat: "jeans",
+
+    }
+    
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+
+
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://i.postimg.cc/25KRnFRK/pngfind-com-80s-png-200007-1.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://i.postimg.cc/7YYGLw9Y/pngfind-com-fashion-png-323866.png",
+
+    },
+    {
+      id:7,
+      img:"https://i.postimg.cc/15jGh8SZ/pngfind-com-jeans-png-153439.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
